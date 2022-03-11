b677c67502fae88daed5b17fddf5accbb049d596
Vishal Kumar <vishalkumar33@hotmail.com>
2022-03-11 13:07:54 +0530

    Project Raw Structure

---
+++ WalE/WalE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WalE
+//
+//  Created by Vishal Kumar on 11/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
